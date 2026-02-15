44d83395420ef0d4a3d8201d1534d038119a9857
Kyle Derby MacInnis <kderbyma@gmail.com>
2026-02-14 20:01:38 -0800

    public

--- server/knexfile.cjs
+++ server/knexfile.cjs
@@ -4,11 +4,11 @@ module.exports = {
     development: {
         client: 'sqlite3',
         connection: {
-            filename: process.env.DB_PATH || path.join(__dirname, 'server', 'data', 'database.sqlite')
+            filename: process.env.DB_PATH || path.join(__dirname, 'data', 'database.sqlite')
         },
         useNullAsDefault: true,
         migrations: {
-            directory: path.join(__dirname, 'server', 'migrations')
+            directory: path.join(__dirname, 'migrations')
         }
     },
 
